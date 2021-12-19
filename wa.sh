bf12631f2072b97b906cf2462bcd3ef6182e554b
Fiqi-Ganz7 <95555507+Fiqi-WibuSoft@users.noreply.github.com>
2021-12-19 02:06:46 +0000

    Create wa.sh

---
+++ wa.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/bash
+#IndonesianHWT
+#@otak_udang08
+#by MrZhu
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°|>"
+echo  $green    "|       Tools Spesial WhatsApp       |"
+echo  $green    "|                                    |"
+echo  $green    "| Author        : FiqiSans           |"
+echo  $green    "| Recode        : FiqiXcode7                |"
+echo  $green    "| My Team       : IndonesianErorTeam |"
+echo  $green    "| FOLLOW IG     : @HYPUTGANZ_        |"
+echo  $green    "| With Love     : Slma Mlni♡           |"
+echo  $blue "<|°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "======================================="
+echo $blue "|| $green 1. Sadap WhatsApp  $blue              ||"
+echo $blue "|| $green 2. Spam WhatsApp       $blue          ||"
+echo $blue "|| $green 3. Nambah Kontak      $blue           ||"
+echo $blue "|| $green 0. Keluar          	$blue      ||"
+echo $blue "======================================="
+echo $blue"╭──[Tinggal Pilih Aja]>"
+read -p"╰───────root@FiqCode7=>" Gans;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+echo
+echo $green"╭──["$red"Masukan Nomor Kamu"$green"]>"
+read -p"╰───────root@FiqCode7=•>" nama;
+sleep 1
+echo
+echo $green"╭──["$red"Masukan Nomor Target"$green"]>"
+read -p"╰───────root@FiqCode7=•>" cewek;
+sleep 1
+echo $green "Sedang melakukan penyadapan"
+sleep 1
+echo $red "Proses..."
+sleep 1
+echo $cyan "10% •"
+sleep 2
+echo $red "20% • •"
+sleep 2
+echo $cyan "30% • • •"
+sleep 2
+echo $red "40% • • • •"
+sleep 2
+echo $cyan "50% • • • • •"
+sleep 2
+echo $red "60% • • • • • •"
+sleep 2
+echo $cyan "70% • • • • • • •"
+sleep 2
+echo $red "80% • • • • • • • •"
+sleep 2
+echo $cyan "90% • • • • • • • • •"
+sleep 2
+echo $red "100% • • • • • • • • • •"
+sleep 2
+echo $green "SUKSES SLURR :v !"
+sleep 2
+echo $red "Data :"
+echo
+echo  $green "Nomor kamu $nama"
+echo $green "Nomor target $cewek"
+sleep 1
+echo $green "Silahkan tunggu 10-15 menit"
+echo $green "Untuk mendapatkan kode verifikasi ilegal"
+echo $green "FiqiSans & FiqiXcode7 In Here"
+fi
+
+if [ $Gans = 2 ] || [ $Gans = 02 ]
+then
+git clone https://github.com/Fiqi-WibuSoft/Sadap-WhatsApp
+echo
+cd prank
+php wa.php
+fi
+
+if [ $Gans = 3 ] || [ $Gans = 03 ]
+then
+echo $blue"╭──[Kontak cewek/cowok]>"
+read -p"╰───────root@FiqCode7=>" nama;
+sleep 1
+echo $red "Loading..."
+sleep 1
+echo $blue "+6281455119351"
+sleep 1
+echo $green "+6285745151698"
+sleep 1
+echo $cyan "+6281250274632"
+sleep 1
+echo $purple "+6285704868189"
+sleep 1
+echo $blue "+6282133059248"
+sleep 1
+echo $red "+6282292789020"
+sleep 1
+echo $green "+6282138152597"
+sleep 1
+echo $cyan "+6281217809109"
+sleep 1
+echo $purple "+628584800635"
+sleep 1
+echo $blue "Terakhir Kontak gw FiqiXcode7"
+sleep 1
+echo $blue "-083125187277-"
+fi
+
+
+if [ $Gans = 0 ] || [ $Gans = 00 ]
+then
+echo  $red "Jangan lupakan nama saya FiqiXcode7"
+echo  $blue "Admin dari Indonesia Hack Team!"
+echo  $green "Sampai jumpa ! JANGAN LUPA FOLLOW INSTAGRAM @hyputganz_ :v"
+exit
+fi
